63589781e536912bd9c45ec251f5061cac64aba2
Martin Kaushal <kaushalmartin@gmail.com>
2024-01-31 00:01:12 +0530

    ssh

--- python_terminal.py
+++ python_terminal.py
@@ -1,35 +1,26 @@
 import paramiko
 
-def ssh_into_server(hostname, port, username, password, command):
-    try:
-         
-        ssh_client = paramiko.SSHClient()
-        # Automatically add the server's host key
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        
-        # Connect to the server
-        ssh_client.connect(hostname, port=port, username=username, password=password)
+hostname="192.168.1.44"
+prt=22
+user="martin"
+passwrd="1234"
 
-        # Run the command on the remote server
-        stdin, stdout, stderr = ssh_client.exec_command(command)
+try:
+    client= paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname,port=prt,username=user,password=passwrd)
 
-        # Print the command output
-        print("Command output:\n", stdout.read().decode("utf-8"))
+    while True:
+        try:
+            cmd=input("$> ")
+            if cmd == "exit" : break
+            stdin,stdout, stderr=client.exec_command(cmd)
+            print(stdout.read().decode())
 
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        # Close the SSH connection
-        ssh_client.close()
+        except KeyboardInterrupt:
+            break
+    client.close()
 
-# Replace these values with your server details
-hostname = "192.168.1.44"
-port = 22  # Default SSH port is 22
-username = "martin"
-password = "1234"
-
-# Example command to run on the remote server
-command_to_run = "ls"
-
-# SSH into the server and run the command
-ssh_into_server(hostname, port, username, password, command_to_run)
+except Exception as err:
+    print(str(err))            
